9654215ea4eac12c85defc712d7c5508c46a897c
polylend-dev <info@polylend.com>
2025-11-20 09:44:46 +0400

    fix bug: invalidate right offer

--- src/PolyLend.sol
+++ src/PolyLend.sol
@@ -254,7 +254,7 @@ contract PolyLend is PolyLendEE, ERC1155TokenReceiver {
         requests[requestId].borrower = address(0);
 
         // invalidate the offer
-        offers[requestId].lender = address(0);
+        offers[_offerId].lender = address(0);
 
         // transfer the borrowers collateral to address(this)
         conditionalTokens.safeTransferFrom(msg.sender, address(this), positionId, collateralAmount, "");
